bb781bdcfdc2e7356cc900c85b21614896ddcaac
Anton Stoychev <antitoxic@gmail.com>
2015-04-17 15:39:27 +0300

    Add EEA logo and new footer settings test.

---
+++ TimeheroesSettings.php
@@ -0,0 +1,18 @@
+<?php
+
+$wgFooterIcons = array(
+  "poweredby" => array(
+     "mediawiki" => array(
+        "src" => null,
+        "url" => "//www.mediawiki.org/",
+        "alt" => "Powered by MediaWiki",
+     )
+  ),
+  "eea" => array(
+     "eea" => array(
+        "src" => '/resources/assets/sponsors/eea.png',
+        "url" => "http://eeagrants.org/",
+        "alt" => "EEA grants",
+     )
+  ),
+);
